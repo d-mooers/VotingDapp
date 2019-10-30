aa8c29a55b837bcc4d4fa627e354bcbd42aaa247
Dylan Mooers <dmooers@calpoly.edu>
2019-10-30 15:19:00 -0700

    Added event for votes, and a func to listen for the event

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,53 @@ pragma solidity ^0.5.8;
 
 contract Election {
 	
-	string public candidate;
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	
+	// Read/Write Candidates
+	mapping(uint => Candidate) public candidates;
+	
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+
+	// Store Candidates Count
+	uint public candidatesCount;
 
 	constructor() public {
 		
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	
+	}
+	
+	event votedEvent (
+		uint indexed _candidateId
+	);
+
+
+	function addCandidate (string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote(uint _candidateID) public {
+		// require that they havent voted before
+		require(!voters[msg.sender]);
+
+		//require a valid candidate
+		require(_candidateID > 0 && _candidateID <= candidatesCount);
+
+		//record that voter has voted
+		voters[msg.sender] = true;
+
+		//update candidate vote count
+		candidates[_candidateID].voteCount++;
+
+		//trigger voted event
+		emit votedEvent(_candidateID);
 	}
 }
